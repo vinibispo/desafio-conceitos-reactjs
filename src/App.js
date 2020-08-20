a2d1a83011770e3459025837a24fa125e1661d93
vinibispo <vini.bispo015@gmail.com>
2020-08-19 23:00:01 -0300

    Finished challenge

--- src/App.js
+++ src/App.js
@@ -1,26 +1,43 @@
 import React from "react";
 
 import "./styles.css";
+import { useState } from "react";
+import { useEffect } from "react";
+import api from "./services/api";
 
 function App() {
+  const [repositories, setRepositories] = useState([])
+  useEffect(() => {
+    api.get('repositories').then(response => setRepositories(response.data))
+  }, [])
   async function handleAddRepository() {
-    // TODO
+    const response = await api.post('repositories', {
+      id: "1",
+      url: "https://github.com/vinibispo",
+      title: "script-de-busca-e-downoad-de-imagens",
+      techs: ["NodeJS"],
+    })
+    setRepositories([...repositories, response.data])
   }
 
   async function handleRemoveRepository(id) {
-    // TODO
+    await api.delete(`/repositories/${id}`)
+    setRepositories(repositories.filter(repo => repo.id !== id))
   }
 
   return (
     <div>
       <ul data-testid="repository-list">
-        <li>
-          Repositório 1
+        {repositories.map(repo => (
+          <li key={repo.id}>
+            {repo.title}
 
-          <button onClick={() => handleRemoveRepository(1)}>
-            Remover
+            <button onClick={() => handleRemoveRepository(repo.id)}>
+              Remover
           </button>
-        </li>
+          </li>
+        ))}
+
       </ul>
 
       <button onClick={handleAddRepository}>Adicionar</button>
